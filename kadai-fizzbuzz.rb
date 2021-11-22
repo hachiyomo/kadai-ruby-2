13b0688b464c6484ddf6e04527b3860565f73105
hachiyomo <yomohachi09@gmail.com>
2021-11-22 17:30:35 +0000

    kadai-fizzbuzz-2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,18 +1,18 @@
-  # メソッドを定義
+# メソッドを定義
 def fizzbuzz(num)
-    if num % 3 == 0 && num % 5 == 0
-      puts "FizzBuzz"
-    elsif num % 3 == 0
-      puts "Fizz"
-    elsif num % 5 == 0
-     puts "Buzz"
-    else 
-     puts num.to_s
-    end
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else 
+    return num.to_s
+  end
 end
 
 # メソッドを呼び出し
 num_max = 100
-  (1..100).each do |num|
-fizzbuzz(num)
+  (1..num_max).each do |num|
+puts fizzbuzz(num)
 end
